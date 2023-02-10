5e56c6c6bd7c908c300243a34bf8d8a5e94dae63
Tales Valente <talesvalente@gmail.com>
2023-02-10 10:59:31 -0300

    RegisterUserNameView
    +Major Update on SRC
    -DROP DUPLICATES FROM USERNAMEVIEW

--- watering/Presentation/RegisterUser/View/Components/UserNameView.swift
+++ watering/Presentation/RegisterUser/View/Components/UserNameView.swift
@@ -1,66 +1,67 @@
+////
+////  NameUser.swift
+////  Watering
+////
+////  Created by Narely Lima on 29/09/22.
+////
 //
-//  NameUser.swift
-//  Watering
+//import Foundation
+//import SwiftUI
 //
-//  Created by Narely Lima on 29/09/22.
+//struct UserNameView : View {
+//    @State var personName: String = ""
+////    let notification = NotificationController()
+//    var body: some View {
+//        ZStack{
+//            Image("Flower")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(alignment: .trailing)
+//            VStack {
+//                Spacer().frame(height: 50)
+//                question
+//                digiteAqui
+//                Divider()
+//                    .background(ThemeEnum.primary)
+//                    .frame(width: 300)
+//                Spacer().frame(height: 50)
+//                navigation
+//            }
+//        }
+//    }
+//    var question: some View {
+//        Text("What is your name?")
+//            .foregroundColor(ThemeEnum.primary)
+//            .font(.system(size: 22, design: .rounded))
 //
-
-import Foundation
-import SwiftUI
-
-struct UserNameView : View {
-    @State var personName: String = ""
-//    let notification = NotificationController()
-    var body: some View {
-        ZStack{
-            ThemeEnum.flower
-                .resizable()
-                .scaledToFit()
-            VStack {
-                Spacer().frame(height: 50)
-                question
-                digiteAqui
-                Divider()
-                    .background(ThemeEnum.primary)
-                    .frame(width: 300)
-                Spacer().frame(height: 50)
-                navigation
-            }
-        }
-    }
-    var question: some View {
-        Text("What is your name?")
-            .foregroundColor(ThemeEnum.primary)
-            .font(.system(size: 22, design: .rounded))
-
-    }
-    var digiteAqui: some View{
-        TextField("Type Here", text: $personName)
-            .multilineTextAlignment(.center)
-            .foregroundColor(.gray)
-            .font(.system(size: 20, design: .rounded))
-            .padding()
-            .onChange(of: personName) { newValue in
-                saveName()
-            }
-    }
-    var navigation: some View {
-        NavigationLink(destination: RegisterPlantNameView().navigationBarHidden(true), label: {
-            Text("Next")
-                .foregroundColor(ThemeEnum.primary)
-                .padding()
-                .frame(width: 200, height: 50)
-                .font(.system(size: 20, design: .rounded))
-                .overlay(RoundedRectangle(cornerRadius: 15)
-                    .stroke(ThemeEnum.primary, lineWidth: 2))
-                .padding()
-                .cornerRadius(20)
-        }
-        )
-    }
-    func saveName() {
-        UserDefaults.standard.setPersonName(value: personName)
-//        notification.requestPermission()
-    }
-
-}
+//    }
+//    var digiteAqui: some View{
+//        TextField("Type Here", text: $personName)
+//            .multilineTextAlignment(.center)
+//            .foregroundColor(.gray)
+//            .font(.system(size: 20, design: .rounded))
+//            .padding()
+//            .onChange(of: personName) { newValue in
+//                saveName()
+//            }
+//    }
+//    var navigation: some View {
+//        NavigationLink(destination: RegisterPlantNameView().navigationBarHidden(true), label: {
+//            Text("Next")
+//                .foregroundColor(ThemeEnum.primary)
+//                .padding()
+//                .frame(width: 200, height: 50)
+//                .font(.system(size: 20, design: .rounded))
+//                .overlay(RoundedRectangle(cornerRadius: 15)
+//                    .stroke(ThemeEnum.primary, lineWidth: 2))
+//                .padding()
+//                .cornerRadius(20)
+//        }
+//        )
+//    }
+//    func saveName() {
+//        UserDefaults.standard.setPersonName(value: personName)
+////        notification.requestPermission()
+//    }
+//
+//}
--- watering/Presentation/RegisterUser/View/RegisterUserNameView.swift
+++ watering/Presentation/RegisterUser/View/RegisterUserNameView.swift
@@ -10,18 +10,92 @@ import SwiftUI
 
 
 struct RegisterUserNameView: View {
+    @State var personName: String = ""
+
     var body: some View {
-        ThemeEnum.secondary.ignoresSafeArea()
         NavigationView {
-            ZStack (alignment: .top){
-                ThemeEnum.secondary.ignoresSafeArea()
-                UserNameView()
-            }
-        }.navigationBarBackButtonHidden(true)
+            ZStack {
+                Image("Flower")
+                    //.resizable()
+                    .fixedSize()
+                    .scaledToFit()
+                    .alignmentGuide(.trailing) { dimension in
+                        dimension[.trailing] - 70
+                    }
+                    .frame(alignment: .trailing)
+                VStack {
+                    Spacer().frame(height: 50)
+
+                    Text("What is your name?")
+                        .foregroundColor(ThemeEnum.primary)
+                        .font(.system(size: 22, design: .rounded))
+                        .fixedSize()
+
+                    TextField("Type Here", text: $personName)
+                        .fixedSize()
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(.gray)
+                        .font(.system(size: 20, design: .rounded))
+                        .padding()
+                        .onChange(of: personName) { newValue in
+                            saveName()
+                        }
+
+
+
+                } //EOV
+                //.frame(width: 400, height:500)
+                //.background(Color.red)
+
+                ZStack {
+                    if (!personName.isEmpty && personName.count >= 5) {
+
+                        Divider()
+                            .background(ThemeEnum.primary)
+                            .frame(width: 300)
+                            .padding(.bottom, 50)
+
+
+                        Spacer()
+                            .frame(height: 50)
+
+                        NavigationLink(destination: RegisterPlantNameView()
+                            .navigationBarHidden(true),
+                               label: {
+                                    Text("Next")
+                                        .foregroundColor(ThemeEnum.primary)
+                                        .padding()
+                                        .frame(width: 200, height: 50)
+                                        .font(.system(size: 20, design: .rounded))
+                                        .overlay(RoundedRectangle(cornerRadius: 15)
+                                            .stroke(ThemeEnum.primary, lineWidth: 2))
+                                        .padding()
+                                        .cornerRadius(20)
+                                }
+                        )
+                        .padding(.top, 50)
+                    }
+
+                }
+                .padding(.top, 200)
+            }//EOZ
+            .background(Color(UIColor.systemBackground))
+            .navigationBarBackButtonHidden(true)
+
+        }
+
     }
-}
-struct RegisterUserNameView_Preview : PreviewProvider {
-    static var previews: some View {
-        RegisterUserNameView()
+
+    func saveName() {
+        UserDefaults.standard.setPersonName(value: personName)
+        //notification.requestPermission()
     }
+
+    struct RegisterUserNameView_Preview : PreviewProvider {
+        static var previews: some View {
+            RegisterUserNameView()
+        }
+    }
+
+
 }
